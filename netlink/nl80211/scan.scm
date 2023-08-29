dc182f8032668a8f1a5919f39a3cb6c48da66bff
Marius Bakke <marius@gnu.org>
2023-08-29 13:10:43 +0800

    scan: Handle missing status.
    * netlink/nl80211/scan.scm (print-scan): Handle the case where STATUS
    is #f.

--- netlink/nl80211/scan.scm
+++ netlink/nl80211/scan.scm
@@ -123,12 +123,12 @@
                              (format #f "~@[(on ~a)~]" (link-index->name ifindex))
                              "")
                          (cond
+                          ((not status) "unknown status")
                           ((= status NL80211_BSS_STATUS_AUTHENTICATED)
                            "authenticated")
                           ((= status NL80211_BSS_STATUS_ASSOCIATED) "associated")
                           ((= status NL80211_BSS_STATUS_IBSS_JOINED) "joined")
-                          (else (format #f "unknown status: ~a"
-                                        (if status status "?")))))
+                          (else (format #f "unknown status: ~a" status))))
                  (print-bss bss)))
              (format (current-error-port) "bss info missing!~%"))))
       ((memv (message-kind message) (list NL80211_CMD_SCAN_ABORTED))
